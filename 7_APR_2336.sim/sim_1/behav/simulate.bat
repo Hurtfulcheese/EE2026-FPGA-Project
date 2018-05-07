637afac55872978638bc4c9cb259da33e6477e16
QzSG <quantumparadoxes@gmail.com>
2018-05-07 18:01:56 +0800

    Cleanup
    :recycle:

--- 7_APR_2336.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\vivadooo\\Vivado\\2016.2\\bin
-call %xv_path%/xsim AUDIO_FX_TOP_behav -key {Behavioral:sim_1:Functional:AUDIO_FX_TOP} -tclbatch AUDIO_FX_TOP.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
